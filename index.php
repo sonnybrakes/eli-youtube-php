cd1f63414a848545681fdcdcab0b9c0e0602b60d
Michel Frechette <mifrec@hotmail.com>
2016-03-09 10:31:28 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- #1 Introduction to php
+#2 Syntax and Errors -->
+
+<!-- this syntax will comment multiple lines of code not inside of php tags -->
+
+<?php
+
+  // two forward slashes will comment a single line of code inside of php tags
+  $string = "hello world";
+  $year = 2016;
+  print "<h1 align=center>$string</h1>";
+  print "<h3 align=center>$year</h3>"
+  // include 'greeting.php';
+
+  
+  /* multiple
+  lines of
+  comments */
+
+ ?>
